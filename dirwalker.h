20b3adb602f16f1ddf95cf4ba22ada62eb442564
Jens Luedicke <jens.luedicke@gmail.com>
2010-08-16 07:25:16 +0200

    Added license headers to source code.

--- dirwalker.h
+++ dirwalker.h
@@ -1,6 +1,12 @@
-/** \file dirwalker.h
-*  dirwalker
-*/
+//  example/main.cpp  ---------------------------------------------------------//
+
+// 
+// Copyright Jens Luedicke 2010
+// 
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+//----------------------------------------------------------------------------// 
 
 #include <iostream>
 #include <map>
