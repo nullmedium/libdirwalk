c57b40e4798c25fe02f9f128bb9f7ee123c25a55
Jens Luedicke <jens.luedicke@gmail.com>
2010-08-18 23:47:07 +0200

    Added fgrep example Renamed main.cpp to dirwalker.cpp

---
+++ example/fgrep.cpp
@@ -0,0 +1,52 @@
+//  example/fgrep.cpp  -------------------------------------------------------//
+
+// 
+// Copyright Jens Luedicke 2010
+// 
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+//----------------------------------------------------------------------------// 
+
+#include <iostream>
+#include <fstream>
+
+#include "dirwalker.h"
+
+struct Grepper {
+    void operator()(const boost::filesystem::path &path) {
+        std::ifstream in(path.string().c_str(), std::ios::in);
+
+        int lineNum = 0;
+
+        while (in.good()) {
+            char line[1024];
+            in.getline(line, 1024);
+
+            ++lineNum;
+
+            if (std::string(line).find(str) != std::string::npos) {
+                std::cout << path.string() << ":" << lineNum << std::endl;
+            }
+        }
+    }
+
+    std::string str;
+};
+
+
+int main(int argc, char *argv[]) {
+    dirwalk::dirwalker dw;
+
+    if (argc > 2) {
+        Grepper grep;
+        grep.str = argv[1];
+
+        dw.doOnFile(grep);
+        dw.walk(boost::filesystem::path(argv[2]));
+    } else {
+        std::cout << "usage: fgrep <pattern> <directory>" << std::endl;
+    }
+    
+    return 0;
+}
